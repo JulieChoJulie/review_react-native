69c749ee888354c3b234072866d3d51e4e20c9e3
JulieChoJulie <h32cho@gmail.com>
2020-03-25 13:57:02 -0400

    pages added

---
+++ screens/reviewDetails.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Details = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Review Deatils</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    }
+})
+
+export default Details;
