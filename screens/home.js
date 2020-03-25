44b3e6670b99041aa259f55cda681bc836d5e67f
JulieChoJulie <h32cho@gmail.com>
2020-03-25 19:30:09 -0400

    navigation options

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-const Home = () => {
+const Home = ({ navigation }) => {
+    const [reviews, setReviews] = useState([
+        { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1'},
+        { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2'},
+        { title: 'Not so "Final" Fantasy', rating: 3, body: 'ipsum', key: '3'},
+    ])
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails')
+        // navigation.push('ReviewDetails');
+    };
+
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.title}>Home</Text>
+            <FlatList
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('ReviewDetails', item)}
+                    >
+                        <Text style={globalStyles.titleText}>{ item.title }</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     );
 };
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-const ReviewDetails = () => {
+const ReviewDetails = ({ navigation }) => {
+    const pressHandler = () => {
+        navigation.goBack();
+    }
+
     return (
         <View style={globalStyles.container}>
-            <Text>Review Deatils</Text>
+            <Text>{ navigation.getParam('title') }</Text>
+            <Text>{ navigation.getParam('rating') }</Text>
+            <Text>{ navigation.getParam('body') }</Text>
         </View>
     );
 };
