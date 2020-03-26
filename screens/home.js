8ff756e385fe270b96184f99d4aa18ef081265d4
JulieChoJulie <h32cho@gmail.com>
2020-03-26 15:45:37 -0400

    modal added

--- screens/home.js
+++ screens/home.js
@@ -1,20 +1,46 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Home = ({ navigation }) => {
+    const [modalOpen, setModalOpen] = useState(false);
     const [reviews, setReviews] = useState([
         { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1'},
         { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2'},
         { title: 'Not so "Final" Fantasy', rating: 3, body: 'ipsum', key: '3'},
-    ])
+    ]);
+
+
     const pressHandler = () => {
         navigation.navigate('ReviewDetails')
     };
 
     return (
         <View style={globalStyles.container}>
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={styles.modalContent}>
+                    <MaterialIcons
+                        name='close'
+                        size={24}
+                        onPress={() => setModalOpen(false)}
+                        style={{
+                            ...styles.modalToggle,
+                            ...styles.modalClose,
+                        }}
+                    />
+                    <Text>Hello From the modal</Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                name='add'
+                size={24}
+                onPress={() => setModalOpen(true)}
+                style={styles.modalToggle}
+            />
+
             <FlatList
                 data={reviews}
                 renderItem={({ item }) => (
@@ -30,5 +56,23 @@ const Home = ({ navigation }) => {
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        borderRadius: 10,
+        padding: 10,
+        alignSelf: 'center',
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0,
+    },
+    modalContent: {
+        flex: 1,
+    }
+})
 
 export default Home;
